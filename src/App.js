a8009c775c4e2eee3807e24bb0f16967ce08fb32
miki.pejic <miki.pejic@yahoo.co.uk>
2022-02-05 19:23:47 +0000

    Added in basic navigation bar

--- src/App.js
+++ src/App.js
@@ -1,18 +1,22 @@
 import { React, useState, useEffect } from 'react';
 import { CustomParallax } from './Components/CustomParallax.js';
+import { Navbar } from './Components/Navbar.js';
 
 import image2 from './assets/image_1.jfif';
 
 function App() {
 
     return (
-      <div style={{ textAlign: 'center' }}>
-        <CustomParallax image={image2}/>
-        <CustomParallax image={image2}/>
-        <CustomParallax image={image2}/>
-        <CustomParallax image={image2}/>
-        <div style={{ height: '100vh' }}></div>
-      </div>
+      <>
+        <Navbar/>
+        <div style={{ textAlign: 'center' }}>
+          <CustomParallax image={image2}/>
+          <CustomParallax image={image2}/>
+          <CustomParallax image={image2}/>
+          <CustomParallax image={image2}/>
+          <div style={{ height: '100vh' }}></div>
+        </div>
+      </>
     );
 }
 
