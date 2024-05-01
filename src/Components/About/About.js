17fc3e2f136b46b0d4dd690c32847f9e0f9cc676
mikipejic <miki.pejic@yahoo.co.uk>
2024-05-01 14:32:49 +0100

    Update CV, update welcome heading

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -24,7 +24,7 @@ export const About = () => {
                 <article className="about__card">
                   <FaAward className="about__icon"/>
                   <h5>Experience</h5>
-                  <small>6+ Years</small>
+                  <small>8+ Years</small>
                 </article>
                 <article className="about__card">
                   <FiUsers className="about__icon"/>
--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './Header.css'
+import './HeaderTopAnimation.css'
 
 import { HeaderButtons } from './HeaderButtons.js';
 import ME from '../../assets/DSC_0064.JPG';
@@ -10,7 +11,21 @@ export const Header = () => {
     return (
         <header>
             <div className="container header__container">
-              <h5>Hello I'm</h5>
+            <div class="content">
+              <div class="content__container">
+                <p class="content__container__text">
+                  Hello
+                </p>
+                
+                <ul class="content__container__list">
+                  <li class="content__container__list__item">world !</li>
+                  <li class="content__container__list__item">people !</li>
+                  <li class="content__container__list__item">users !</li>
+                  <li class="content__container__list__item">everybody !</li>
+                </ul>
+              </div>
+            </div>
+              <h5>I'm</h5>
               <h1>Miki</h1>
               <h5 className="text-light">Fullstack Developer</h5>
 
