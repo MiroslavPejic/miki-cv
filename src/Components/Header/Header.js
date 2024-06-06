60cd78c98b1c3d1e7b6d1a59f0c16a703fe5df79
mikipejic <miki.pejic@yahoo.co.uk>
2024-06-06 10:36:06 +0100

    Remove fancy text

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -10,35 +10,21 @@ import { HeaderSocials } from './HeaderSocials.js';
 export const Header = () => {
     return (
         <header>
-            <div className="container header__container">
-            <div class="content">
-              <div class="content__container">
-                <p class="content__container__text">
-                  Hello
-                </p>
-                
-                <ul class="content__container__list">
-                  <li class="content__container__list__item">world !</li>
-                  <li class="content__container__list__item">people !</li>
-                  <li class="content__container__list__item">users !</li>
-                  <li class="content__container__list__item">everybody !</li>
-                </ul>
-              </div>
+          <div className="container header__container">
+            
+            <h5>I'm</h5>
+            <h1>Miki</h1>
+            <h5 className="text-light">Fullstack Developer</h5>
+
+            <HeaderButtons />
+            <HeaderSocials/>
+
+            <div className="me__image">
+              <img src={ME} alt="Me" />
             </div>
-              <h5>I'm</h5>
-              <h1>Miki</h1>
-              <h5 className="text-light">Fullstack Developer</h5>
 
-              <HeaderButtons />
-              <HeaderSocials/>
-
-              <div className="me__image">
-                <img src={ME} alt="Me" />
-              </div>
-
-              <a href="#contact" className="scroll__down">Scroll Down</a>
-
-            </div>
+            <a href="#contact" className="scroll__down">Scroll Down</a>
+          </div>
         </header>
     )
 }
